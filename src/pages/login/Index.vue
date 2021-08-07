f40d32c1b60c72ee8631e2c073c73fa4042aaa1f
zhong wang <zhongwang07@yahoo.com>
2021-08-07 23:45:51 +0100

    antd-admin

--- src/pages/login/Index.vue
+++ src/pages/login/Index.vue
@@ -1,20 +1,33 @@
 <template>
-  <a-form :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol">
+  <a-form ref="formRef" :label-col="labelCol" :wrapper-col="wrapperCol">
     <a-form-item label="Username">
       <a-input type="text" />
     </a-form-item>
     <a-form-item label="Password">
       <a-input type="password" />
     </a-form-item>
-    <a-form-item label="Login">
-      <a-button type="submit" />
+    <a-form-item :wrapper-col="{ span: 14, offset: 4 }">
+      <a-button type="primary" @click="onSubmit">Login</a-button>
     </a-form-item>
   </a-form>
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue'
-export default defineComponent({})
+import { defineComponent, ref } from 'vue'
+export default defineComponent({
+  setup () {
+    const formRef = ref()
+    const onSubmit = () => {
+      console.log('login')
+    }
+    return {
+      labelCol: { span: 4 },
+      wrapperCol: { span: 14 },
+      formRef,
+      onSubmit
+    }
+  }
+})
 </script>
 
 <style>
